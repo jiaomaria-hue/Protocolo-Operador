e81cbdcdde5dc5eb46fe09118b3b29260e4d49f5
jiaomaria-hue <jiaomaria@Gmail.com>
2026-05-28 22:08:23 -0300

    Adicionando atividade.py

--- projeto/todos meus projetos/projeto.py
+++ projeto/todos meus projetos/projeto.py
@@ -1,2 +1,4 @@
-nome = input('qual e seu nome? ') 
-print(f'É um prazer te conhecer, {nome}!')
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite mais um numero: '))
+total = n1 + n2
+print('a soma entre {} e {} vale {}'.format(n1, n2, total))
