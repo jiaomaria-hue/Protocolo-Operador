e777851a7525b42ba373da66d20524cafda70fed
jiaomaria-hue <jiaomaria@Gmail.com>
2026-05-29 15:49:19 -0300

    concluido ex05: sucesso e antecessor

---
+++ projeto/todos meus projetos/mundo_1/exercicio005.py
@@ -0,0 +1,14 @@
+#n1 = int(input('um valor: '))
+#n2 = int(input('digite outro valor: '))
+#s = n1 + n2
+#m = n1 * n2
+#d = n1 / n2 
+#dm = n1 // n2
+#e = n1 ** n2
+#rint(f'a soma e {s},o produto é {m} e a divisao é {d :.3f}'), end=''
+#print(f'divisao inteira {dm}, potencia {e}')
+
+n1 = int(input('fale um valor.'))
+
+print(f'o sucessor de {n1} é o {n1 + 2} e o antecessor é o {n1 - 1}.')
+
