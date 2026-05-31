b352942be4a499d940f02696f20ee754e113c43c
jiaomaria-hue <jiaomaria@Gmail.com>
2026-05-31 13:45:09 -0300

    atualizando atualizaçoes

--- projeto/todos meus projetos/mundo_1/exercicio006.py
+++ projeto/todos meus projetos/mundo_1/exercicio006.py
@@ -1,2 +1,2 @@
 n1 = float(input('Escreva um numero: '))
-print(f'o numero {n1}, multiplicado por 3 é igual a {n1 * 3}, é multiplicado por 2 e igual a {n1 * 2}, e divido e igual a {n1**0.5 :3f}')
+print(f'o numero {n1}, multiplicado por 3 é igual a {n1 * 3}, é multiplicado por 2 e igual a {n1 * 2}, \na raiz quadrada e igual a {n1**0.5:.3f}')
