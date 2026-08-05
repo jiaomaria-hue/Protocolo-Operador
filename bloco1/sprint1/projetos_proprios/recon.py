3c5729a7296e01df66d35d251ae4fc19f0d6c0ef
jiaomaria-hue <jiaomaria@Gmail.com>
2026-08-05 20:41:27 -0300

    Atualizando mais ainda o scan.py

--- bloco1/sprint1/projetos_proprios/recon.py
+++ bloco1/sprint1/projetos_proprios/recon.py
@@ -1,10 +1,14 @@
 import socket
+from rich import print
+from rich.panel import Panel
 class Recon:
     def __init__(self, alvo):
         self.alvo = alvo
     def resolver_ip(self):
         try:
+            
             ip = socket.gethostbyname(self.alvo)
-            print(f'✅ IP encontrado: {ip}')
-        except:
-            print('❌ Não foi possível resolver o domínio')
+            conteudo = f'''[green]✅ IP encontrado:[/] [red]{ip}[/]'''
+            print(Panel(conteudo, title='[red]IP HOST[/]', width=32))
+        except socket.gaierror:
+            print('[red]❌ Não foi possível resolver o domínio[/]')
